67fddd7e016ef25e0a1f432c22452327b9d576eb
123 <17620835317@163.com>
2019-03-11 15:04:39 +0800

    修改style

--- Home/script/dataList.js
+++ Home/script/dataList.js
@@ -5,7 +5,7 @@ Mock.mock(
             {
                 "image": './image/buy-pro1.jpg',
                 "title": "iphone 6plus 128G",
-                "price": "3200.000",
+                "price": "3200.00",
                 "id": '1'
             },
             {
@@ -50,54 +50,54 @@ Mock.mock(
                 "price": "1200.00",
                 "id": '8'
             },
-            // {
-            //             //     "image": './image/buy-pro1.jpg',
-            //             //     "title": "iphone 6plus 128G",
-            //             //     "price": "3200.000",
-            //             //     "id": '9'
-            //             // },
-            //             // {
-            //             //     "image": './image/shuma04.jpg',
-            //             //     "title": "iphone xs max 128G",
-            //             //     "price": "5800.00",
-            //             //     "id": '10'
-            //             // },
-            //             // {
-            //             //     "image": './image/shoubiao.jpg',
-            //             //     "title": "阿玛尼机械表",
-            //             //     "price": "2000.00",
-            //             //     "id": '11'
-            //             // },
-            //             // {
-            //             //     "image": './image/shuma03.jpg',
-            //             //     "title": "全新佳能80D相机",
-            //             //     "price": "5600.00",
-            //             //     "id": '12'
-            //             // },
-            //             // {
-            //             //     "image": './image/yifu.jpg',
-            //             //     "title": "新款Supreme棒球衣",
-            //             //     "price": "520.00",
-            //             //     "id": '13'
-            //             // },
-            //             // {
-            //             //     "image": './image/kuzi.jpg',
-            //             //     "title": "新款Supreme休闲裤",
-            //             //     "price": "220.00",
-            //             //     "id": '14'
-            //             // },
-            //             // {
-            //             //     "image": './image/yumaoqiu.jpg',
-            //             //     "title": "红双喜羽毛球拍",
-            //             //     "price": "300.00",
-            //             //     "id": '15'
-            //             // },
-            //             // {
-            //             //     "image": './image/qiuxie.jpg',
-            //             //     "title": "麦迪鸳鸯球鞋",
-            //             //     "price": "1200.00",
-            //             //     "id": '16'
-            //             // }
+            {
+                "image": './image/buy-pro1.jpg',
+                "title": "iphone 6plus 128G",
+                "price": "3200.00",
+                "id": '9'
+            },
+            {
+                "image": './image/shuma04.jpg',
+                "title": "iphone xs max 128G",
+                "price": "5800.00",
+                "id": '10'
+            },
+            {
+                "image": './image/shoubiao.jpg',
+                "title": "阿玛尼机械表",
+                "price": "2000.00",
+                "id": '11'
+            },
+            {
+                "image": './image/shuma03.jpg',
+                "title": "全新佳能80D相机",
+                "price": "5600.00",
+                "id": '12'
+            },
+            {
+                "image": './image/yifu.jpg',
+                "title": "新款Supreme棒球衣",
+                "price": "520.00",
+                "id": '13'
+            },
+            {
+                "image": './image/kuzi.jpg',
+                "title": "新款Supreme休闲裤",
+                "price": "220.00",
+                "id": '14'
+            },
+            {
+                "image": './image/yumaoqiu.jpg',
+                "title": "红双喜羽毛球拍",
+                "price": "300.00",
+                "id": '15'
+            },
+            {
+                "image": './image/qiuxie.jpg',
+                "title": "麦迪鸳鸯球鞋",
+                "price": "1200.00",
+                "id": '16'
+            }
         ],
         "desc": "ok",
         "msg": "请求成功",
